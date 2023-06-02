bab8e378213ef4d091c5c1b6d8ef9de96ccc7be3
Daniela Liendo <daniela@ualabee.com>
2023-06-02 19:35:25 +0200

    mini map page component was created

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-mini-map',
+  templateUrl: './mini-map.component.html',
+  styles: []
+})
+
+export class MiniMapComponent {}
