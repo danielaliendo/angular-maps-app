f27d50ff3ebcb31d2d8f33b81b539f9f7acea8b9
Daniela Liendo <daniela@ualabee.com>
2023-06-02 20:01:49 +0200

    path was added

--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapsLayoutComponent } from "./layouts/maps-layout/maps-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MapsLayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
