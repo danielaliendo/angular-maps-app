7545463005288790af5de531ef8cc8e03b1f9014
Daniela Liendo <daniela@ualabee.com>
2023-06-06 20:36:44 +0200

    mini map component was created

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {LngLat, Map, Marker} from "mapbox-gl";
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
-  styles: []
+  styles: [`
+    div {
+      width: 100%;
+      height: 150px;
+      margin: 0;
+    }
+  `]
 })
 
-export class MiniMapComponent {}
+export class MiniMapComponent implements AfterViewInit {
+
+  @ViewChild('map') mapRef?: ElementRef
+  @Input() lngLat?: [number, number]
+
+  ngAfterViewInit(): void {
+    if (!this.mapRef?.nativeElement) throw 'map div not found'
+    if (!this.lngLat) throw 'lngLat can NOT be null'
+
+    const map = new Map({
+      container: this.mapRef?.nativeElement, // ref element
+      style: 'https://api.maptiler.com/maps/basic-v2/style.json?key=isuv7z5lVm7U4p8srIAd', // style URL
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map)
+
+  }
+
+
+}
