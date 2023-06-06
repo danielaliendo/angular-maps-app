f9c4481414d8b7e4f3a87f703088bb9594595a72
Daniela Liendo <daniela@ualabee.com>
2023-06-06 20:37:13 +0200

    full-page page component was created

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Map} from 'mapbox-gl';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styles: []
+  styles: [`
+    div {
+      width: 100vw;
+      height: 100vh;
+    }
+  `]
 })
 
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+
+  @ViewChild('map') mapRef?: ElementRef
+
+  constructor() {
+  }
+
+  ngAfterViewInit(): void {
+
+    if (!this.mapRef?.nativeElement) return
+    const map = new Map({
+      container: this.mapRef?.nativeElement, // container ID
+      style: 'https://api.maptiler.com/maps/basic-v2/style.json?key=isuv7z5lVm7U4p8srIAd', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+}
+
